fa0d4390e1077e79742102af1685d141b903f4cd
Edouard Etancelin <etancelin.edouard@gmail.com>
2017-04-15 11:10:38 +0200

    Make SearchBox FC statefull

--- client/app/bundles/HelloWorld/components/SearchBox.jsx
+++ client/app/bundles/HelloWorld/components/SearchBox.jsx
@@ -2,19 +2,26 @@ import React from 'react';
 import Immutable from 'immutable';
 import TagList from './TagList';
 
-const SearchBox = () => (
-  <div>
-
-    <div className="search-bar">
-      <input
-        className="search-input"
-        type="text"
-        name="search"
-        placeholder="Enter you're tag"
-        autoFocus="true"
-      />
-    </div>
-    <TagList tags={Immutable.fromJS(['1', '2', '3'])} />
-  </div>
-);
+class SearchBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { userInput: props.value };
+  }
+  render() {
+    return (
+      <div>
+        <div className="search-bar">
+          <input
+            className="search-input"
+            type="text"
+            name="search"
+            placeholder="Enter you're tag"
+            autoFocus="true"
+          />
+        </div>
+        <TagList tags={Immutable.fromJS(['1', '2', '3'])} />
+      </div>
+    );
+  }
+}
 export default SearchBox;
