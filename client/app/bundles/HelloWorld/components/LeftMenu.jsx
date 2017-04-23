c046d44ce213a2574be7fd1d2bc04ebcacb30ef6
16Q45T <etancelin.edouard@gmail.com>
2017-04-23 16:57:26 +0200

    Delete an Unsudes Html Erb file. And create a left panel jsx component

---
+++ client/app/bundles/HelloWorld/components/LeftMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftMenu = () => (
+    <div style={{width: '200px', backgroundColor: '#FAFAFA'}}>
+      <ul>
+        <li>Coucou</li>
+        <li> hello</li>
+      </ul>
+    </div>
+)
+export default LeftMenu ;
--- client/app/bundles/HelloWorld/components/MainContent.jsx
+++ client/app/bundles/HelloWorld/components/MainContent.jsx
@@ -1,29 +1,24 @@
 import React, { PropTypes } from 'react';
 import VisibleBookmarkList from '../containers/VisibleBookmarkList'
 import SearchBoxContainer from '../containers/SearchBoxContainer'
+import LeftMenu from './LeftMenu'
 
 const HomePage = ({ name, updateName }) => (
-
-  <div style={{display: 'flex', flexDirection:'row', backgroundColor: '#FAFAFA', justifyContent: 'center', minHeight: '100vh'}}>
-  <div style={{width: '200px', backgroundColor: '#FAFAFA'}}>
-    <ul>
-      <li>Coucou</li>
-      <li> hello</li>
-    </ul>
+    <div style={{display: 'flex', flexDirection:'row', backgroundColor: '#FAFAFA', justifyContent: 'center', minHeight: '100vh'}}>
+    <LeftMenu />
+    <div style={{ width: '596px', padding: '15px', backgroundColor: 'white'}}>
+      <SearchBoxContainer />
+      <div className="section_overdue">
+      <h2 className="header">En retard </h2>
+      <ul className="items priority_list">
+        <li className="task_item item_76105126 not_shared menu_clickable priority_1 agenda_item indent_1" >
+        <div className="invisible_space"></div>
+          <VisibleBookmarkList />
+        </li>
+      </ul>
+      </div>
+    </div>
   </div>
-  <div style={{ width: '596px', padding: '15px', backgroundColor: 'white'}}>
-    <SearchBoxContainer />
-    <div className="section_overdue">
-    <h2 className="header">En retard </h2>
-    <ul className="items priority_list">
-    <li className="task_item item_76105126 not_shared menu_clickable priority_1 agenda_item indent_1" >
-      <div className="invisible_space"></div>
-      <VisibleBookmarkList />
-    </li>
-</ul>
-</div>
-  </div>
-</div>
 );
 
 HomePage.propTypes = {
