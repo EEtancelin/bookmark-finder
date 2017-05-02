96a3e5480a6c94a5b4bfb3e7b0a1e8f7f64de624
16Q45T <etancelin.edouard@gmail.com>
2017-05-02 14:04:59 +0200

    Clean MainContent

--- client/app/bundles/HelloWorld/components/MainContent.jsx
+++ client/app/bundles/HelloWorld/components/MainContent.jsx
@@ -10,12 +10,8 @@ const HomePage = ({ name, updateName }) => (
       <SearchBoxContainer />
       <div className="section_overdue">
       <h2 className="header">En retard </h2>
-      <ul className="items priority_list">
-        <li className="task_item item_76105126 not_shared menu_clickable priority_1 agenda_item indent_1" >
         <div className="invisible_space"></div>
           <VisibleBookmarkList />
-        </li>
-      </ul>
       </div>
     </div>
   </div>
