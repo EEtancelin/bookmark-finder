ad8d186615f2257ed07d624c06cf0992995ba90d
16Q45T <etancelin.edouard@gmail.com>
2017-05-03 11:48:45 +0200

    Implement show bookmark Tag.

--- app/controllers/api/v1/bookmark_tags_controller.rb
+++ app/controllers/api/v1/bookmark_tags_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::BookmarkTagsController < Api::V1::BaseController
+
+  before_action :set_bookmark_tag, only: [ :show ]
   def index
     @bookmark_tags = policy_scope(BookmarkTag)
     @bookmark_tags = BookmarkTag.all
+  end
+
+  private
+  def set_bookmark_tag
+    @bookmark_tag = BookmarkTag.find(params[:id])
+    authorize @bookmark_tag  # For Pundit
   end
 end
