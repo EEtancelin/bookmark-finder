de975b2db108003c44d70e9d727241973de0bdd6
16Q45T <etancelin.edouard@gmail.com>
2017-05-03 11:43:43 +0200

    Implement a API show endpoint for tag.

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,7 +1,21 @@
-# app/controllers/api/v1/restaurants_controller.rb
+# app/controllers/api/v1/tags_controller.rb
 class Api::V1::TagsController < Api::V1::BaseController
+
+  before_action :set_tag, only: [ :show ]
+
   def index
     @tags = policy_scope(Tag)
     @tags = Tag.all
+  end
+
+  def show
+    @tags = policy_scope(Tag)
+    @tags = Tag.first
+  end
+
+  private
+  def set_tag
+    @tag = Tag.find(params[:id])
+    authorize @tag  # For Pundit
   end
 end
---
+++ app/views/api/v1/bookmark_tags/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @bookmark_tag, :id, :uuid, :tag_id, :bookmark_id
