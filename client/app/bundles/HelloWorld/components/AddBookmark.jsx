9f4e93a7a5a418033dd466311f3abf2c4f52fddc
16Q45T <etancelin.edouard@gmail.com>
2017-06-05 11:12:49 +0200

    Create a Add Bookmark Component.

---
+++ client/app/bundles/HelloWorld/components/AddBookmark.jsx
@@ -0,0 +1,52 @@
+// Absolute Import
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Form } from 'semantic-ui-react';
+
+
+// Component constants
+// Style constants
+
+// Component
+class AddBookmark extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: '', url: '' };
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onUrlChange = this.onUrlChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  onTitleChange(e) { this.setState({ title: e.target.value }); }
+  onUrlChange(e) { this.setState({ url: e.target.value }); }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({ title: '', url: '' });
+    this.props.onSubmit(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        { this.props.show &&
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Field>
+            <label>Title</label>
+            <input placeholder='First Name'  onChange={this.onTitleChange} value={this.state.title} />
+          </Form.Field>
+          <Form.Field>
+            <label>Url</label>
+            <input placeholder='Last Name' onChange={this.onUrlChange} value={this.state.url} />
+          </Form.Field>
+          <Button type='submit' >save</Button>
+        </Form>
+      }
+      </div>
+    )};
+}
+
+AddBookmark.propTypes = {
+
+};
+
+export default AddBookmark;
---
+++ client/app/bundles/HelloWorld/containers/AddBookmarkContainer.jsx
@@ -0,0 +1,25 @@
+
+import { connect } from 'react-redux';
+import AddBookmark from '../components/AddBookmark';
+import { createBookmark } from '../actions/bookmarkActionCreators';
+
+// Which part of the Redux global state does our component want to receive as props?
+
+
+const mapStateToProps = (state) => {
+  return {
+    show: state.getIn(['ui', 'showAddBookmarkForm']),
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownprops ) => {
+  return {
+    onSubmit: (values) => { dispatch(createBookmark(values)); },
+  };
+};
+
+
+// Don't forget to actually use connect!
+// Note that we don't export HelloWorld, but the redux "connected" version of it.
+// See https://github.com/reactjs/react-redux/blob/master/docs/api.md#examples
+export default connect(mapStateToProps, mapDispatchToProps)(AddBookmark);
