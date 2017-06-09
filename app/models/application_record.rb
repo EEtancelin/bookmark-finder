b0ae1e1e85bbfd2e189d0a9ca745b81cd61fec5e
16Q45T <etancelin.edouard@gmail.com>
2017-06-09 22:01:09 +0200

    Refactor the application_record controller and the page controller.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
+def to_redux_entitie
+  [self.id.to_s, self.to_redux_hash]
+end
+
   def to_redux_hash
     attr = self.attributes.to_h
     attr.each_key do |k|
@@ -11,10 +15,9 @@ class ApplicationRecord < ActiveRecord::Base
     return attr
   end
 
-
 #Given a attribute name return true if attribute name is "id" or finish by "_id"
   def attribute_is_id_or_foreign_key(attr_name)
-    attr_name.to_s.end_with?('id') || attr_name.to_s.end_with?('_id')
+    attr_name == 'id' || attr_name.to_s.end_with?('_id')
   end
-  
+
 end
