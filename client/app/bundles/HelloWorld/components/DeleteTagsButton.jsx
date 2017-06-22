f5585ca85ebcd09713295b8ef9d21de87c2b8919
16Q45T <etancelin.edouard@gmail.com>
2017-06-22 21:47:26 +0200

    Change cursor to pointer on the Delete searched tags button.

--- client/app/bundles/HelloWorld/components/DeleteTagsButton.jsx
+++ client/app/bundles/HelloWorld/components/DeleteTagsButton.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Icon from './Icons';
 
+const style = { marginRight: '3px', cursor: 'pointer'};
 const DeleteTagsButton = ({ deleteTagsFn }) => (
-  <div className ="align-center" onClick={deleteTagsFn} style={{ marginRight: '3px' }} >
+  <div className ="align-center" onClick={deleteTagsFn} style={style} >
       <Icon icon={'bin2'}  viewBox={'0 0 1024 1024'} color={'#555555'} />
   </div>
 )
