49d0f4a9b555d5a349cbbc308adaf6c1f2034b1d
16Q45T <etancelin.edouard@gmail.com>
2017-08-01 17:34:02 +0200

    Clean the entities Reducer Delte A lot of Unused Function.

--- client/app/bundles/HelloWorld/reducers/entitiesReducer.jsx
+++ client/app/bundles/HelloWorld/reducers/entitiesReducer.jsx
@@ -20,53 +20,6 @@ export const getProposedBookmarksTagsTitles = (state) => {
   );
 };
 
-// Which Tag have one bookmark in Common with the searchedTags ?
-export const getTagsIdsAssociateToBookmarks = (bookmarkTag, bookmarks) => {
-  return (getBookmarksIdsForEachTag(bookmarkTag)
-    .filter(bookmarkTags => !bookmarkTags.intersect(bookmarks).isEmpty())
-    .keySeq()
-  );
-};
-
-// Which tags have one bookmark in common with the provided tags ?
-export const getTagsIdsAssociateToTags = (bookmarkTag, tags) => {
-  return (getTagsIdsAssociateToBookmarks(
-      bookmarkTag, getBookmarksIdsAssociateToTags(bookmarkTag, tags)
-  )
-  );
-};
-
-export const getTagsIdsForEachBookmark = (bookmarkTag) => {
-  return (bookmarkTag
-    .groupBy(bt => bt.get('bookmark_id'))
-    .map(x => x.map(y => y.get('tag_uuid')).toSet())
-  );
-};
-
-export const getBookmarksIdsForEachTag = (bookmarkTag) => {
-  return (bookmarkTag
-    .groupBy(bt => bt.get('tag_uuid'))
-    .map(x => x.map(y => y.get('bookmark_id')).toSet())
-  );
-};
-
-
-export const isNewBookmark = (state, title) => {
-  return (!(state.hasIn(['tags', 'title'], title)));
-};
-
-// Selector
-export const getTagsByBookmark = (bookmarkTag, bookmarkId) => {
-  return (getTagsIdsForEachBookmark(bookmarkTag).get(bookmarkId));
-};
-
-export const getBookmarksIdsAssociateToTags = (bookmarkTag, tags) => {
-  return (getTagsIdsForEachBookmark(bookmarkTag)
-    .filter(bookmarkTags => bookmarkTags.isSuperset(tags))
-    .keySeq()
-  );
-};
-
 const entitiesReducer = combineReducers({
   bookmarks,
   teams,
