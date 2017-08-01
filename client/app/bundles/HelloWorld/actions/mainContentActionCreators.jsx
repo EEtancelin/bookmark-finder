bea7506d2628a5f95a85c8d09e1facaf132be4c9
16Q45T <etancelin.edouard@gmail.com>
2017-08-01 16:58:44 +0200

    Clean React On Rails unused Boilerplate.

--- client/app/bundles/HelloWorld/actions/mainContentActionCreators.jsx
+++ client/app/bundles/HelloWorld/actions/mainContentActionCreators.jsx
@@ -1,12 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 
-import { HELLO_WORLD_NAME_UPDATE } from '../constants/appConstants';
-
-export const updateName = (text) => ({
-  type: HELLO_WORLD_NAME_UPDATE,
-  text,
-});
-
 export const showAddBookmarkForm = () => ({
   type: 'SHOW_ADD_BOOKMARK_FORM',
 });
